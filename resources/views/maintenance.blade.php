eacac14e9f552b240c35d1cf6ad74dbe99e0df62
Gabriel Canali <gabrielcanali.dev@gmail.com>
2023-08-31 22:36:24 -0300

    feat: add maintenance page

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Gabriel Canali">
+    <meta name="description" content="Aplicativo de gerenciamento de jogos de RPG">
+    <title>RPG Manager - Em desenvolvimento</title>
+    <style>
+        body { background-color: #dbdbdb; display: flex; justify-content: center; align-items: center; width: 100vw; height: 100vh; }
+        body { font: 1.5rem Helvetica, sans-serif; color: #333; }
+        h1 { font-size: 3rem; }
+        main { text-align: center; margin: 0 auto; max-width: 100%; }
+        @media screen and (max-width: 400px) {
+            h1 { display: none; }
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <h1>Em desenvolvimento</h1>
+        <div>
+            <p>Lamento por essa inconveniência mas este site ainda está em desenvolvimento.</p>
+            <p>Retorne em breve para mais novidades!</p>
+            <!-- <p>&mdash; Gabriel Canali</p> -->
+        </div>
+    </main>
+</body>
+</html>
