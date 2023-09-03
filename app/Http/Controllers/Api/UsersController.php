2249a38a9465811063720a87e15474d27a38b070
Gabriel Canali <gabrielcanali.dev@gmail.com>
2023-09-03 17:54:22 -0300

    feat: add Users controller to API

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -16,14 +16,6 @@ class UsersController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -35,14 +27,6 @@ class UsersController extends Controller
      * Display the specified resource.
      */
     public function show(Users $users)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Users $users)
     {
         //
     }
