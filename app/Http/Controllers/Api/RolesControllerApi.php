936a9b228daba70ff80cfc34fc6b0b9bf3c4e390
Gabriel Canali <gabrielcanali.dev@gmail.com>
2024-08-21 18:07:13 -0300

    feat: add role feature initial files

---
+++ app/Http/Controllers/Api/RolesControllerApi.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Roles;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RolesControllerApi extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Roles $roles)
+    {
+        //
+    }
+}
