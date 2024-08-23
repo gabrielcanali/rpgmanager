6f97be191cb03e82b4c5c93ada120978f6a65711
Gabriel Canali <gabrielcanali.dev@gmail.com>
2024-08-22 23:11:11 -0300

    fix: change assets folder and vite config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
-                'resources/js/app.js',
+                'resources/assets/',
             ],
-            refresh: true,
+            refresh: [
+                'resources/'
+            ],
         }),
     ],
 });
